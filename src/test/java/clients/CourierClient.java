b8a5c23b7d8de358dc2396a0bbe9d9af5a76249e
Artyom <tema.dolganov@yandex.ru>
2025-07-20 22:22:38 +0300

    firstCommit

--- src/test/java/clients/CourierClient.java
+++ src/test/java/clients/CourierClient.java
@@ -1,81 +1,43 @@
 package clients;
 
-
-
+import io.qameta.allure.Step;
 import io.restassured.response.Response;
-
 import models.Courier;
-
 import models.CourierCredentials;
-
-
-
 import static io.restassured.RestAssured.given;
 
-
-
 public class CourierClient {
-
     private static final String BASE_URI = "https://qa-scooter.praktikum-services.ru";
-
     private static final String CREATE_PATH = "/api/v1/courier";
-
     private static final String LOGIN_PATH = "/api/v1/courier/login";
-
     private static final String DELETE_PATH = "/api/v1/courier/{id}";
 
-
-
-
-
+    @Step("Создать нового курьера")
     public Response create(Courier courier) {
-
         return given()
-
                 .baseUri(BASE_URI)
-
                 .header("Content-type", "application/json")
-
                 .body(courier)
-
                 .when()
-
                 .post(CREATE_PATH);
-
     }
 
-
-
+    @Step("Авторизовать курьера")
     public Response login(CourierCredentials creds) {
-
         return given()
-
                 .baseUri(BASE_URI)
-
                 .header("Content-type", "application/json")
-
                 .body(creds)
-
                 .when()
-
                 .post(LOGIN_PATH);
-
     }
 
-
-
+    @Step("Удалить курьера с ID {id}")
     public Response delete(int id) {
-
         return given()
-
                 .baseUri(BASE_URI)
-
                 .pathParam("id", id)
-
                 .when()
-
                 .delete(DELETE_PATH);
-
     }
-
 }
--- src/test/java/clients/OrderClient.java
+++ src/test/java/clients/OrderClient.java
@@ -1,57 +1,40 @@
 package clients;
 
-
-
+import io.qameta.allure.Step;
 import io.restassured.response.Response;
-
 import models.Order;
-
-
-
 import static io.restassured.RestAssured.given;
 
-
-
 public class OrderClient {
-
     private static final String BASE_URI = "https://qa-scooter.praktikum-services.ru";
-
     private static final String CREATE_PATH = "/api/v1/orders";
-
     private static final String LIST_PATH = "/api/v1/orders";
+    private static final String CANCEL_PATH = "/api/v1/orders/cancel";
 
-
-
-
-
+    @Step("Создать новый заказ")
     public Response create(Order order) {
-
         return given()
-
                 .baseUri(BASE_URI)
-
                 .header("Content-type", "application/json")
-
                 .body(order)
-
                 .when()
-
                 .post(CREATE_PATH);
-
     }
 
-
-
+    @Step("Получить список заказов")
     public Response getList() {
-
         return given()
-
                 .baseUri(BASE_URI)
-
                 .when()
-
                 .get(LIST_PATH);
-
     }
 
+    @Step("Отменить заказ с trackId {trackId}")
+    public Response cancelOrder(int trackId) {
+        return given()
+                .baseUri(BASE_URI)
+                .queryParam("track", trackId)
+                .when()
+                .put(CANCEL_PATH);
+    }
 }
